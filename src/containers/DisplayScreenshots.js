c926eb8b37e80c96194c2fff457c5369de68b7fb
tonnamb <tonnam.balankura@gmail.com>
2017-04-08 19:02:12 -0400

    display top two probabilities

--- src/containers/DisplayScreenshots.js
+++ src/containers/DisplayScreenshots.js
@@ -9,12 +9,52 @@ class DisplayScreenshots extends Component {
 
   renderScreenshots () {
     return this.props.screenshots.map((image) => {
+      const topTwo = this.getTopTwo(this.getAvgScores(image.emotions))
       return (
         <li key={image.id}>
           <img src={image.src} alt='screenshot' />
+          <p>1. {topTwo.first.key} = {(topTwo.first.value * 100).toFixed(0)} %</p>
+          <p>2. {topTwo.second.key} = {(topTwo.second.value * 100).toFixed(0)} %</p>
         </li>
       )
     })
+  }
+
+  getTopTwo (scores) {
+    let firstKey, secondKey
+    let firstValue = 0
+    let secondValue = 0
+    Object.entries(scores).forEach(([key, value]) => {
+      if (value > firstValue) {
+        secondValue = firstValue
+        secondKey = firstKey
+        firstValue = value
+        firstKey = key
+      } else if (value > secondValue) {
+        secondValue = value
+        secondKey = key
+      }
+    })
+    return {
+      first: {key: firstKey, value: firstValue},
+      second: {key: secondKey, value: secondValue}
+    }
+  }
+
+  getAvg (emotions, key) {
+    let sum = 0
+    emotions.forEach(obj => {
+      sum += obj.scores[key]
+    })
+    return sum / emotions.length
+  }
+
+  getAvgScores (emotions) {
+    let avgScore = {}
+    for (let emo in emotions[0].scores) {
+      avgScore[emo] = this.getAvg(emotions, emo)
+    }
+    return avgScore
   }
 
   render () {
