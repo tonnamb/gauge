92f5a3a48ebc22be44202bec18ce9cb2f939f777
tonnamb <tonnam.balankura@gmail.com>
2017-05-22 20:34:59 -0400

    removed key

--- src/reducers/apiKeyReducer.js
+++ src/reducers/apiKeyReducer.js
@@ -1,6 +1,6 @@
 import { CHANGE_API_KEY } from '../actions/types'
 
-const apiKeyReducer = (state = '886ff18f3a0b44588a9e3d10dba801ee', action) => {
+const apiKeyReducer = (state = '', action) => {
   switch (action.type) {
     case CHANGE_API_KEY:
       return action.apiKey
